e8da1e9a97b8210298ebc1610c2911008ced5bf9
Craig D. Miller <cmiller@srri.umass.edu>
2018-01-01 12:50:56 -0500

    fix bugs in update4_fromMasterWWL_to_AT_MacMini.sh, now it actually works, probably for the first time

--- lib/updatesToWWL/update4_fromMasterWWL_to_AT_MacMini.sh
+++ lib/updatesToWWL/update4_fromMasterWWL_to_AT_MacMini.sh
@@ -5,21 +5,32 @@
 #
 # make a local copy called SonosLibrary on DharaWWL silver 1gb portable HD, making slight rearrangements
 PORTABLE='/Volumes/DharaWWL'
-WWL_FOLDER='WWL_2017'
-VMC_ADDITIONAL ='VMC_Additional_Files'
-MP3_FOLDER='/Users/Shared/MP3\ Library'
+WWL_FOLDER='WWL_2017/WWL_mp3'
+VMC_ADDITIONAL='VMC_Additional_Files'
 
-# copy library and delete old stuff including VMC_ADDTIONAL
-mkdir -v $MP3_FOLDER
-rsync -avz --delete --exclude=.DS_Store $PORTABLE/$WWL_FOLDER/WWL_mp3/* $MP3_FOLDER
+LOCALSHAREDFOLDER='/Users/Shared'
+MP3_FOLDER='MP3 Library'
 
-# then add in VMC_ADDITIONAL
-mkdir -v $MP3_FOLDER/$VMC_ADDITIONAL
-rsync -avz --exclude=.DS_Store $PORTABLE/$VMC_ADDITIONAL/* $MP3_FOLDER/$VMC_ADDITIONAL
+# make MP3_FOLDER and VMC_ADDITIONAL 
+mkdir -v "${LOCALSHAREDFOLDER}/${MP3_FOLDER}"
+mkdir -v "${LOCALSHAREDFOLDER}/${MP3_FOLDER}/${VMC_ADDITIONAL}"
+
+# temporarily move VMC_ADDITIONAL out of MP3_FOLDER to avoid being deleted in next step)
+mv "${LOCALSHAREDFOLDER}/${MP3_FOLDER}/${VMC_ADDITIONAL}" "${LOCALSHAREDFOLDER}/${VMC_ADDITIONAL}"
+
+# sync VMC_Additional_Files and delete old stuff
+echo "Copying from: ${PORTABLE}/${VMC_ADDITIONAL}/*   to   ${LOCALSHAREDFOLDER}/${VMC_ADDITIONAL}"
+rsync -avz --delete --exclude=.DS_Store --exclude=._* "${PORTABLE}/${VMC_ADDITIONAL}"/* "${LOCALSHAREDFOLDER}/${VMC_ADDITIONAL}"
+
+# sync MP3_FOLDER and delete old stuff
+echo "Copying from: ${PORTABLE}/${WWL_FOLDER}/*   to   ${LOCALSHAREDFOLDER}/${MP3_FOLDER}"
+rsync -avz --delete --exclude=.DS_Store --exclude=._* "${PORTABLE}/${WWL_FOLDER}"/* "${LOCALSHAREDFOLDER}/${MP3_FOLDER}"
+
+echo "moving VMC_Additional_Files back into MP3_FOLDER"
+mv "${LOCALSHAREDFOLDER}/${VMC_ADDITIONAL}" "${LOCALSHAREDFOLDER}/${MP3_FOLDER}/${VMC_ADDITIONAL}"
 
 # make files read-only for AT user
-chmod -R g-w $MP3_FOLDER
-chmod -R o-w $MP3_FOLDER
+chmod -R go-w "${LOCALSHAREDFOLDER}/${MP3_FOLDER}"
 
 echo 'Now ALL ORIGINAL FILES should be on local HD'
 echo 'Done.  Log in as AT and verify files are readable but not changeable.'
